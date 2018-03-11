e079dcf32c82ba0e671c999ebb0f775e7eecb674
Joe Ibershoff <joe@ibershoff.com>
2018-03-10 20:12:12 -0500

    allow late audio capture

--- index.js
+++ index.js
@@ -1,45 +1,52 @@
-var audioElements = document.getElementsByTagName('audio'),
-    sounds = {};
-
-for (var i = 0; i < audioElements.length; i++) {
-    sounds[audioElements[i].id] = audioElements[i];
-}
 
 // Solves chrome for andriod issue 178297 Require user gesture
 // https://code.google.com/p/chromium/issues/detail?id=178297
 // Fix based on code from http://blog.foolip.org/2014/02/10/media-playback-restrictions-in-blink/
 if (mediaPlaybackRequiresUserGesture()) {
-    window.addEventListener('keydown', removeBehaviorsRestrictions);
-    window.addEventListener('mousedown', removeBehaviorsRestrictions);
-    window.addEventListener('touchstart', removeBehaviorsRestrictions);
+  window.addEventListener('keydown', removeBehaviorsRestrictions);
+  window.addEventListener('mousedown', removeBehaviorsRestrictions);
+  window.addEventListener('touchstart', removeBehaviorsRestrictions);
 }
 
 function mediaPlaybackRequiresUserGesture() {
-    // test if play() is ignored when not called from an input event handler
-    var video = document.createElement('video');
-    video.play();
-    return video.paused;
+  // test if play() is ignored when not called from an input event handler
+  var video = document.createElement('video');
+  video.play();
+  return video.paused;
 }
 
 function removeBehaviorsRestrictions() {
-    for (var i = 0; i < audioElements.length; i++) {
-        audioElements[i].load();
-    }
+  for (var soundId in sounds) {
+    sounds[soundId].load();
+  }
 
-    window.removeEventListener('keydown', removeBehaviorsRestrictions);
-    window.removeEventListener('mousedown', removeBehaviorsRestrictions);
-    window.removeEventListener('touchstart', removeBehaviorsRestrictions);
+  window.removeEventListener('keydown', removeBehaviorsRestrictions);
+  window.removeEventListener('mousedown', removeBehaviorsRestrictions);
+  window.removeEventListener('touchstart', removeBehaviorsRestrictions);
 }
 
 function playSound(sound) {
-    for (var key in sounds) {
-      sounds[key].pause();
-    }
+  for (var key in sounds) {
+    sounds[key].pause();
+  }
 
-    sounds[sound].load();
-    sounds[sound].play();
+  sounds[sound].load();
+  sounds[sound].play();
 }
 
+var sounds = {};
+
+function captureAudioElements() {
+  var audioElements = document.getElementsByTagName('audio');
+
+  for (var i = 0; i < audioElements.length; i++) {
+    sounds[audioElements[i].id] = audioElements[i];
+  }
+}
+
+captureAudioElements();
+
 module.exports = {
-    playSound: playSound
+  playSound: playSound,
+  captureAudioElements: captureAudioElements
 };
